dd27d3ddbd3b86435a1157e34d95a661f86ac0ac
Savely Domnikov <savely.domnikov@yandex.ru>
2023-05-21 22:20:45 +0300

    Initial commit

---
+++ src/main/java/com/savely/courseproject2/CourseProject2Application.java
@@ -0,0 +1,13 @@
+package com.savely.courseproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseProject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/savely/courseproject2/CourseProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savely.courseproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
